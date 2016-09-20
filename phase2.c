32e0ee0f317ad6e5eb747e47b17ecaaaa97254ff
Yikai <yikaicao27@gmail.com>
2016-09-20 09:04:40 -0700

    Check_kernel_mode, disableItrpt, enableItrp added.

---
+++ phase2.c
@@ -0,0 +1,161 @@
+/* ------------------------------------------------------------------------
+   phase2.c
+
+   University of Arizona
+   Computer Science 452
+
+   ------------------------------------------------------------------------ */
+
+#include <phase1.h>
+#include <phase2.h>
+#include <usloss.h>
+
+#include "message.h"
+
+/* ------------------------- Prototypes ----------------------------------- */
+int start1 (char *);
+extern int start2 (char *);
+
+/* ------------------------- Added Functions ----------------------------------- */
+void enableInterrupts();
+void disableInterrupts();
+void check_kernel_mode(char *arg);
+
+
+
+/* -------------------------- Globals ------------------------------------- */
+
+int debugflag2 = 0;
+
+// the mail boxes 
+mailbox MailBoxTable[MAXMBOX];
+
+// also need array of mail slots, array of function ptrs to system call 
+// handlers, ...
+
+
+
+
+/* -------------------------- Functions ----------------------------------- */
+
+/* ------------------------------------------------------------------------
+   Name - start1
+   Purpose - Initializes mailboxes and interrupt vector.
+             Start the phase2 test process.
+   Parameters - one, default arg passed by fork1, not used here.
+   Returns - one to indicate normal quit.
+   Side Effects - lots since it initializes the phase2 data structures.
+   ----------------------------------------------------------------------- */
+int start1(char *arg)
+{
+    int kid_pid, status;    // instance variables
+    
+    if (DEBUG2 && debugflag2)
+        USLOSS_Console("start1(): at beginning\n");
+
+    check_kernel_mode("start1");
+
+    // Disable interrupts
+    disableInterrupts();
+
+    // Initialize the mail box table, slots, & other data structures.
+    // Initialize USLOSS_IntVec and system call handlers,
+    // allocate mailboxes for interrupt handlers.  Etc... 
+
+    enableInterrupts();
+
+    // Create a process for start2, then block on a join until start2 quits
+    if (DEBUG2 && debugflag2)
+        USLOSS_Console("start1(): fork'ing start2 process\n");
+    kid_pid = fork1("start2", start2, "start2", 4 * USLOSS_MIN_STACK, 1);
+    if ( join(&status) != kid_pid ) {
+        USLOSS_Console("start2(): join returned something other than ");
+        USLOSS_Console("start2's pid\n");
+    }
+
+    return 0;
+} /* start1 */
+
+
+/* ------------------------------------------------------------------------
+   Name - MboxCreate
+   Purpose - gets a free mailbox from the table of mailboxes and initializes it 
+   Parameters - maximum number of slots in the mailbox and the max size of a msg
+                sent to the mailbox.
+   Returns - -1 to indicate that no mailbox was created, or a value >= 0 as the
+             mailbox id.
+   Side Effects - initializes one element of the mail box array. 
+   ----------------------------------------------------------------------- */
+int MboxCreate(int slots, int slot_size)
+{
+    return -1;
+} /* MboxCreate */
+
+
+/* ------------------------------------------------------------------------
+   Name - MboxSend
+   Purpose - Put a message into a slot for the indicated mailbox.
+             Block the sending process if no slot available.
+   Parameters - mailbox id, pointer to data of msg, # of bytes in msg.
+   Returns - zero if successful, -1 if invalid args.
+   Side Effects - none.
+   ----------------------------------------------------------------------- */
+int MboxSend(int mbox_id, void *msg_ptr, int msg_size)
+{
+    return -1;
+} /* MboxSend */
+
+
+/* ------------------------------------------------------------------------
+   Name - MboxReceive
+   Purpose - Get a msg from a slot of the indicated mailbox.
+             Block the receiving process if no msg available.
+   Parameters - mailbox id, pointer to put data of msg, max # of bytes that
+                can be received.
+   Returns - actual size of msg if successful, -1 if invalid args.
+   Side Effects - none.
+   ----------------------------------------------------------------------- */
+int MboxReceive(int mbox_id, void *msg_ptr, int msg_size)
+{
+    return -1;
+} /* MboxReceive */
+
+
+
+/* ------------------------- enableInterrupts ----------------------------------- */
+void enableInterrupts()
+{
+    // turn the interrupts on if we are in kernel mode
+    if( (USLOSS_PSR_CURRENT_MODE & USLOSS_PsrGet()) == 0 ) {
+        //not in kernel mode
+        USLOSS_Console("Kernel Error: Not in kernel mode, may not ");
+        USLOSS_Console("disable interrupts\n");
+        USLOSS_Halt(1);
+    } else
+        //We ARE in kernel mode
+        USLOSS_PsrSet( USLOSS_PsrGet() | USLOSS_PSR_CURRENT_INT );
+}
+
+/* ------------------------- disableInterrupts ----------------------------------- */
+void disableInterrupts()
+{
+    // turn the interrupts OFF iff we are in kernel mode
+    if( (USLOSS_PSR_CURRENT_MODE & USLOSS_PsrGet()) == 0 ) {
+        //not in kernel mode
+        USLOSS_Console("Kernel Error: Not in kernel mode, may not ");
+        USLOSS_Console("disable interrupts\n");
+        USLOSS_Halt(1);
+    } else
+        // We ARE in kernel mode
+        USLOSS_PsrSet( USLOSS_PsrGet() & ~USLOSS_PSR_CURRENT_INT );
+} /* disableInterrupts */
+
+/* ------------------------- check_kernel_mode ----------------------------------- */
+void check_kernel_mode(char *arg)
+{
+    if (!(USLOSS_PSR_CURRENT_MODE & USLOSS_PsrGet()))
+    {
+        USLOSS_Console("%s(): called while in user mode. Halting...\n", arg);
+    }
+} /* check_kernel_mode */
+
