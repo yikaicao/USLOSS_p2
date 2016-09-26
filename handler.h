0bfe9fb4999707f1e06b5c8c60123e5bc8f6b45e
Yikai <yikaicao27@gmail.com>
2016-09-26 10:41:35 -0700

    handler.h added.

---
+++ handler.h
@@ -0,0 +1,5 @@
+extern void nullsys(systemArgs *args);
+extern void clockHandler2(int dev, void *arg);
+extern void diskHandler(int dev, void *arg);
+extern void termHandler(int dev, void *arg);
+extern void syscallHandler(int dev, void *arg);
